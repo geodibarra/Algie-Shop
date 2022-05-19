d32b8041dcdace0e44a7f792093f7ac515fbf361
geodibarra <101815184+geodibarra@users.noreply.github.com>
2022-05-19 10:00:43 +0700

    Add files via upload

---
+++ Algie Shop/Algie.js
@@ -0,0 +1,176 @@
+let menu = document.querySelector('#menu-bar');
+let navbar = document.querySelector('.navbar');
+let header = document.querySelector('.header-2');
+
+menu.addEventListener('click', () =>{
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+});
+
+window.onscroll = () =>{
+    menu.classList.remove('fa-times');
+    navbar.classList.remove('active');
+
+    if(window.scrollY > 150){
+        header.classList.add('active');
+    }else{
+        header.classList.remove('active');
+    }
+
+}
+
+let countDate = new Date('june 1, 2021 00:00:00').getTime();
+
+function CountDown(){
+
+    let now = new Date().getTime();
+    gap = countDate - now;
+
+    let second = 1000;
+    let minute = second * 60;
+    let hour = minute * 60;
+    let day = hour * 24;
+
+    let d = Math.floor(gap / (day));
+    let h = Math.floor((gap % (day)) / (hour));
+    let m = Math.floor((gap % (hour)) / (minute));
+    let s = Math.floor((gap % (minute)) / (second));
+
+    document.getElementById('day').innerText = d;
+    document.getElementById('hour').innerText = h;
+    document.getElementById('minute').innerText = m;
+    document.getElementById('second').innerText = s;
+
+}
+
+setInterval(function(){
+    CountDown();
+},1000)
+
+
+// ***************************************************
+// Shopping Cart functions
+
+var shoppingCart = (function () {
+    // Private methods and properties
+    var cart = [];
+
+    function Item(name, price, count) {
+        this.name = name
+        this.price = price
+        this.count = count
+    }
+
+    function saveCart() {
+        localStorage.setItem("shoppingCart", JSON.stringify(cart));
+    }
+
+    function loadCart() {
+        cart = JSON.parse(localStorage.getItem("shoppingCart"));
+        if (cart === null) {
+            cart = []
+        }
+    }
+
+    loadCart();
+
+
+
+    // Public methods and properties
+    var obj = {};
+
+    obj.addItemToCart = function (name, price, count) {
+        for (var i in cart) {
+            if (cart[i].name === name) {
+                cart[i].count += count;
+                saveCart();
+                return;
+            }
+        }
+
+        console.log("addItemToCart:", name, price, count);
+
+        var item = new Item(name, price, count);
+        cart.push(item);
+        saveCart();
+    };
+
+    obj.setCountForItem = function (name, count) {
+        for (var i in cart) {
+            if (cart[i].name === name) {
+                cart[i].count = count;
+                break;
+            }
+        }
+        saveCart();
+    };
+
+
+    obj.removeItemFromCart = function (name) { // Removes one item
+        for (var i in cart) {
+            if (cart[i].name === name) { // "3" === 3 false
+                cart[i].count--; // cart[i].count --
+                if (cart[i].count === 0) {
+                    cart.splice(i, 1);
+                }
+                break;
+            }
+        }
+        saveCart();
+    };
+
+
+    obj.removeItemFromCartAll = function (name) { // removes all item name
+        for (var i in cart) {
+            if (cart[i].name === name) {
+                cart.splice(i, 1);
+                break;
+            }
+        }
+        saveCart();
+    };
+
+
+    obj.clearCart = function () {
+        cart = [];
+        saveCart();
+    }
+
+
+    obj.countCart = function () { // -> return total count
+        var totalCount = 0;
+        for (var i in cart) {
+            totalCount += cart[i].count;
+        }
+
+        return totalCount;
+    };
+
+    obj.totalCart = function () { // -> return total cost
+        var totalCost = 0;
+        for (var i in cart) {
+            totalCost += cart[i].price * cart[i].count;
+        }
+        return totalCost.toFixed(2);
+    };
+
+    obj.listCart = function () { // -> array of Items
+        var cartCopy = [];
+        console.log("Listing cart");
+        console.log(cart);
+        for (var i in cart) {
+            console.log(i);
+            var item = cart[i];
+            var itemCopy = {};
+            for (var p in item) {
+                itemCopy[p] = item[p];
+            }
+            itemCopy.total = (item.price * item.count).toFixed(2);
+            cartCopy.push(itemCopy);
+        }
+        return cartCopy;
+    };
+
+    // ----------------------------
+    return obj;
+})();
